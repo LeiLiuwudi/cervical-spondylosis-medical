4e5e50a4c26a0de4327f7ecee853d1aa2f221608
LeiLiuwudi <2364485120@qq.com>
2021-09-13 09:59:38 +0800

    first commit

---
+++ src/test/java/com/zju/edu/gcs/CervicalSpondylosisMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zju.edu.gcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervicalSpondylosisMedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
